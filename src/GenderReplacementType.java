4fec5c7605a23de789349f527151bbb25cf1d987
Giacomo Fenzi <giacomofenzi@outlook.com>
2017-03-07 11:58:03 +0000

    Implemented Rule up to 9

--- src/GenderReplacementType.java
+++ src/GenderReplacementType.java
@@ -5,5 +5,6 @@ public enum GenderReplacementType
 {
     MALE,
     FEMALE,
-    UNCHANGED
+    UNCHANGED,
+    INVALID,
 }
--- src/NumberReplacementType.java
+++ src/NumberReplacementType.java
@@ -5,5 +5,6 @@ public enum  NumberReplacementType
 {
     SINGULAR,
     PLURAL,
-    UNCHANGED
+    UNCHANGED,
+    INVALID
 }
